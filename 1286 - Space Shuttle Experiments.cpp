58ba5900b9064e4770f7c4fff54e28ada4a1e57a
Sarwar Jahan <Sarwar05@users.noreply.github.com>
2019-03-07 06:13:22 +0600

    Update 1286 - Space Shuttle Experiments.cpp

--- 1286 - Space Shuttle Experiments.cpp
+++ 1286 - Space Shuttle Experiments.cpp
@@ -1,97 +1,125 @@
-/// First of all, we connect the source point and the experiment, the capacity is the corresponding income,
-/// the edge is connected between the instrument and the sink, the capacity is the carrying cost, and then
-/// the edge is connected between the corresponding experiment and the instrument.
-/// For experimental income (maximum value can also be used, as long as greater than or equal to the
-/// experimental income can be guaranteed), the meaning of the maximum flow at this time is the cost that the
-/// transportation instrument can pay. What is the cost to pay? That is, if certain instruments are used to make profits,
-/// then the cost of carrying these instruments will be lost. If they cannot be profitable,
-/// they will pay the fullest amount of the corresponding experiment's income, and no more (guaranteed not to lose money).
-/// For profitable projects, revenue minus costs is naturally a profit. For unprofitable projects, revenue minus the cost
-/// is equal to 0 (because you said that you paid all the revenues before). The significance is that you do not do this
-/// experiment (losing money at a loss). . So, the result is total revenue minus maximum flow
-///
 #include<bits/stdc++.h>
 using namespace std;
-#define mx 202
-#define inf 1000000009
-vector<int> adj[mx];
-int cap[mx][mx],passed[mx][mx],par[mx];
-int source = 0,sink = 201;
-bool bfs()
+#define ll long long
+#define inf 1e18
+#define vi vector<int>
+/// https://en.wikipedia.org/wiki/Max-flow_min-cut_theorem#Application
+struct Edge
 {
-    memset(par,-1,sizeof par);
+    int from;
+    int to;
+    ll cap;
+    ll flow=0;
+    Edge(int _from, int _to, ll _cap)
+    {
+        from = _from;
+        to = _to;
+        cap = _cap;
+    }
+};
+struct Dinic
+{
+    vector<Edge> edges;
+    vector<vi> adj;
+    int n, m=0;
+    int source,sink;
+    vi level, ptr;
     queue<int> q;
-    q.push(source);
-    par[source] = -5;
-    while(!q.empty()){
-        int from =q.front();
-        q.pop();
-        for(int i=0; i<adj[from].size(); i++){
-            int to = adj[from][i];
-            if(par[to]==-1 && cap[from][to] > passed[from][to]){
-                par[to] =   from;
-                if(to == sink) return true;
+    Dinic(int _n, int _source, int _sink)
+    {
+        n = _n+5;
+        source = _source;
+        sink = _sink;
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+    void addEdge(int from, int to, ll cap)
+    {
+        edges.push_back(Edge(from, to, cap));
+        adj[from].push_back(m++);
+        edges.push_back(Edge(to, from, 0));
+        adj[to].push_back(m++);
+    }
+    bool bfs()
+    {
+        while(!q.empty()){
+            int from = q.front();
+            q.pop();
+            for(int i=0; i<adj[from].size(); i++){
+                int id = adj[from][i];
+                int to = edges[id].to;
+                if(edges[id].cap - edges[id].flow ==0)
+                    continue;
+                if(level[ to ] != -1) continue;
+                level[to] = level[from] + 1;
                 q.push(to);
             }
         }
+        return level[sink] != -1;
     }
-    return false;
-}
-int findMaxFlow()
-{
-    int maxFlow = 0, flow;
-    while(bfs()){
-        flow = inf;
-        int now = sink;
-        while(now!=source){
-            int prev = par[now];
-            flow = min(flow, cap[prev][now] - passed[prev][now]);
-            now = prev;
+    ll dfs(int from, ll pushed)
+    {
+        if(pushed==0) return 0;
+        if(from==sink) return pushed;
+        for(int& cid = ptr[from]; cid<(int)adj[from].size(); cid++){
+            int id = adj[from][cid];
+            int to = edges[id].to;
+            if(level[from]+1 != level[to] || edges[id].cap - edges[id].flow == 0)
+                continue;
+            ll tempFlow = dfs(to, min(pushed, edges[id].cap - edges[id].flow));
+            if(tempFlow==0) continue;
+            edges[id].flow +=tempFlow;
+            edges[id^1].flow -=tempFlow;
+            return tempFlow;
         }
-        maxFlow+=flow;
-        now = sink;
-        while(now!=source){
-            int prev = par[now];
-            passed[prev][now]+=flow;
-            passed[now][prev]-=flow;
-            now = prev;
-        }
+        return 0;
     }
-    return maxFlow;
-}
+    ll getFlow()
+    {
+        //for(int i=0; i<m; i++) edges[i].flow = 0;
+        ll maxFlow = 0;
+        while(true){
+            fill(level.begin(), level.end(), -1);
+            level[source] = 0;
+            while(!q.empty()) q.pop();
+            q.push(source);
+            if(bfs()==false) break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while(ll pushed = dfs(source, inf))
+                maxFlow+=pushed;
+        }
+        return maxFlow;
+    }
+};
+
 int main()
 {
-    int tc,n,m,a,b,c,cs=1,q;
-    scanf("%d",&tc);
-    while(tc--){
-        for(int i=0; i<mx; i++)adj[i].clear();
-        memset(cap,0,sizeof cap);
-        memset(passed,0,sizeof passed);
-        scanf("%d%d",&m,&n);
-        int total = 0;
+    int tc, cs=1;
+    scanf("%d", &tc);
+    while(tc--)
+    {
+        int m,n,a,x;
+        scanf("%d%d", &m,&n);
+        int src = 0, snk = n + (2*m) + 2;
+        Dinic dinic(n+m*2, src, snk);
+        int ans = 0;
         for(int i=1; i<=m; i++){
-            scanf("%d",&a);
-            cap[source][i] = a;
-            adj[source].push_back(i);
-            adj[i].push_back(source);
-            total+=a;
+            scanf("%d", &a);
+            dinic.addEdge(src,i,a);
+            ans+=a;
         }
         for(int i=1; i<=n; i++){
-            scanf("%d",&a);
-            cap[100+i][sink] = a;
-            adj[100+i].push_back(sink);
-            adj[sink].push_back(100+i);
+            scanf("%d", &a);
+            dinic.addEdge(m+i, snk,a);
         }
-        for(int j=1; j<=m; j++){
-            scanf("%d",&q);
-            while(q--){
-                scanf("%d",&a);
-                cap[j][100+a] = inf;
-                adj[j].push_back(100+a);
-                adj[100+a].push_back(j);
+        for(int i=1; i<=m; i++){
+            scanf("%d", &x);
+            while(x--){
+                scanf("%d", &a);
+                dinic.addEdge(i, m+a, inf);
             }
         }
-        printf("Case %d: %d\n",cs++,total-findMaxFlow());
+        printf("Case %d: %d\n", cs++, ans - (int)dinic.getFlow());
     }
-    return 0;
 }
